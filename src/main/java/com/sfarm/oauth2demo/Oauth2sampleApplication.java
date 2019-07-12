eb7bd9ca4e619e735c3ab57c9b353acb4a458c4f
zerocyber <zeroforever@naver.com>
2019-07-12 17:46:28 +0900

    first commit

---
+++ src/main/java/com/sfarm/oauth2demo/Oauth2sampleApplication.java
@@ -0,0 +1,13 @@
+package com.sfarm.oauth2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2sampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2sampleApplication.class, args);
+	}
+
+}
